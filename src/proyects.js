d4492b07389ef8ab3e4c3457202e9e91c0ff4e14
IagoSiccardi <iago.siccardi@gmail.com>
2023-01-19 15:34:57 -0300

    img fixed

--- src/proyects.js
+++ src/proyects.js
@@ -2,14 +2,14 @@ module.exports = [
   {
     title: "Sunset",
     desc: "E-commerce de hoodies oversizes, con capacidad de añadir al carrito, de realizar busquedas en tiempo real, con validaciones de usuario,entre otras caracteristicas.",
-    img: "/static/media/sunset.f5a6f8ebc9acc31af3b4.png",
-    git: "https://github.com/IagoSiccardi/Sunset22/tree/develop/Sunset",
+    img: "sunset.png",
+    git: "https://github.com/IagoSiccardi/Sunset22",
     tech: ["mySQL","Express","EJS","Sequelize" ]
   },
   {
     title: "Jordan Delta",
     desc: "Este proyecto se baso en utilizar las tecnologías de MidJourney y ChapGPT para asi generar los textos e imagenes. En el sitio se intenta representar el lanzamiento de unas nuevas zapatillas Nike Jordan.",
-    img: '/static/media/jordanDelta.09e7cc0548d71e938838.png',
+    img: 'jordanDelta.png',
     git: "https://github.com/IagoSiccardi/IA-Proyect",
     tech: ["React"]
   },
