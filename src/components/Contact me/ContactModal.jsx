5c69942c4a833fe0550fb3e7b2def6b27ffa5f95
iagoSiccardi <iago.siccardi@gmail.com>
2023-04-10 00:04:42 -0300

    Text-area style done

--- src/components/Contact me/ContactModal.jsx
+++ src/components/Contact me/ContactModal.jsx
@@ -5,9 +5,12 @@ import {AiOutlineClose} from 'react-icons/ai'
 const ContactModal = ({setModal,modal,theme}) => {
   return (
     <section className={`contact-modal bg-opacity-20 ${theme ? "bg-black" : "bg-white" }`}>
-        <div className='modal'>
-        <AiOutlineClose color='white' size='2rem' onClick={() => setModal(!modal)}/>
-            hola
+        <div className={`modal ${theme ? "bg-white" : "bg-black"}`}>
+        <AiOutlineClose color={`${theme ? "black" : "white"}`} size='2rem' onClick={() => setModal(!modal)}/>
+            <form action="">
+              <textarea className={`text-area bg-opacity-5 ${theme ? "bg-transparent text-black" : "bg-transparent text-white" }`} cols="30" rows="10"></textarea>
+              <button className='form-button'><p className={` ${theme ? " text-black" : "text-white" }`}>Send</p></button>
+            </form>
         </div>
     </section>
   )
